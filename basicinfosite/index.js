f5e2bc1fd91049a60d8d3e7201727eb7a9bf5592
Jacob Odden <jacob.l.odden@gmail.com>
2019-07-08 09:46:04 -0400

    feat(basic-info-site): finish basic info site project

--- basicinfosite/index.js
+++ basicinfosite/index.js
@@ -1,4 +1,32 @@
+const http = require('http')
 const url = require('url')
 const fs = require('fs')
 
-// NOTE(jacob): basic init stuff for project
+const routes = new Map([
+  ['/', '/index.html'],
+  ['/about', '/about.html'],
+  ['/contact-me', '/contact-me.html'],
+])
+
+http.createServer((req, res) => {
+  const q = url.parse(req.url, true)
+  const page = `${__dirname}/${routes.get(q.path) || '404.html'}`
+  let isErrorPage = false
+
+  fs.readFile(page, (err, data) => {
+    if(err) {
+      res.writeHead(404, {'Content-Type': 'text/html'})
+      return res.end('404 not fonud')
+    }
+
+    if (isErrorPage) {
+      res.writeHead(404, { 'Content-Type': 'text/html' })
+      res.write(data)
+      return res.end('404 not fonud')
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.write(data)
+      return res.end()
+    }
+  })
+}).listen(8080)
